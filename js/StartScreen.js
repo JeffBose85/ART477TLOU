771ee8dc09726dd99156b887d74d08c5cdb377e9
JeffreyBose <jbose8500@gmail.com>
2023-05-01 13:14:17 -0500

    Small adjustments

--- js/StartScreen.js
+++ js/StartScreen.js
@@ -1,6 +1,14 @@
 $(document).click(function() {
+    MoveNext();
+});
+
+$(document).on("keypress", function() {
+    MoveNext();
+});
+
+function MoveNext() {
     $('.container').animate({opacity: '0%'},1900);
     setTimeout(function() {
         window.location.href = "timeline.html";
     }, 2200);
-});
+}
