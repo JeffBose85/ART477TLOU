36f2790af619b2e2bc28e30e36d9c3b6b2ff99a3
Jeffrey Bose <jbose8500@gmail.com>
2023-05-11 02:45:17 -0500

    Added Ellie, Sarah, Tommy, Fireflies pages

--- js/Back.js
+++ js/Back.js
@@ -1,15 +1,12 @@
 $(document).ready(function() {
     $('.back').click(function() {
-        if(window.length > 0) {
-            $('body').animate({opacity: '0%'},1900);
-            setTimeout(function() {
-                history.back();
-            }, 2200);
-        } else {
-            $('body').animate({opacity: '0%'},1900);
-            setTimeout(function() {
-                window.location.href = 'timeline.html';
-            }, 2200);
-        }
+        $('body').animate({opacity: '0%'},1900);
+        setTimeout(function() {
+            history.go(-1);
+        }, 2200);
+
+        setTimeout(function() {
+            window.location.href = 'timeline.html';
+        }, 2400);
     });
 });
